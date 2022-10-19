96764f17f72e6dd20490674e0a731a752735fc36
eoghanruss <114760727+eoghanruss@users.noreply.github.com>
2022-10-19 09:34:01 +0100

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,82 @@
+[
+  {
+    "text": "Always Study Hard",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Remember your CSS",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Javascript can be difficult",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "This is a quote",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Fate is in your hands",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Be the person you want to be",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Study at TUS",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Never half do a  job",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Life is a learning experience",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Never put yourself down",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Peace comes from within",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "War is bad",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "We can only learn to love by loving.",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Change can daunting but always good in the end",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "This is another quote",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Live in the present",
+    "author": null
+  },
+  {
+    "text": "Don't judge other people, only yourself",
+    "author": null
+  },
+  {
+    "text": "Every man dies",
+    "author": null
+  },
+  {
+    "text": "To lead people walk behind them.",
+    "author": "Eoghan Russell"
+  },
+  {
+    "text": "Having nothing, nothing can he lose.",
+    "author": "Eoghan Russell"
+  }
+]
